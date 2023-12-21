f0cee22f39bd774ab11960d40600dccc89a93681
MochArisRofii <mochrofii827@gmail.com>
2023-12-21 08:39:13 +0700

    data_siswa

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
         <div class="">
-            <a class="navbar-brand text-white" href="/data_siswa"><img src="images/white-128.png" width="30" height="30"></a>
+            <a class="navbar-brand text-white" href="/data_siswa"><img src="images/white-128.png" width="34" height="30"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
